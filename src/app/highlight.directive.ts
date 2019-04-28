63c56ee3fce1b74d1ee7f1c51f1df94a52a9f4d6
Lorddoyo <johnkojo7777@gmail.com>
2019-04-28 13:05:40 +0300

    made a HostListener annotation function in my highlight directive

--- src/app/highlight.directive.ts
+++ src/app/highlight.directive.ts
@@ -1,10 +1,20 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef,HostListener } from '@angular/core';
 
 @Directive({
   selector: '[appHighlight]'
 })
 export class HighlightDirective {
 
-  constructor() { }
+  constructor(private elem:ElementRef) {
+    this.elem.nativeElement.style.backgroundColor='green'
+  }
 
-}
+  @HostListener('click') onClicks() {
+    this.highLight('red');
+  }
+
+
+  private highLight(action:string){
+    this.elem.nativeElement.style.backgroundColor=action;
+   }
+  }
