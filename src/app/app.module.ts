6699a2a7a570a31214d688558a91efeba8bec305
Lorddoyo <johnkojo7777@gmail.com>
2019-04-28 11:44:26 +0300

    made a component called repeat

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RepeatComponent } from './repeat/repeat.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RepeatComponent
   ],
   imports: [
     BrowserModule,
