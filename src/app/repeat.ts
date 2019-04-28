2bdc676272efb14ea6723549aa5ca04da1dd1750
Lorddoyo <johnkojo7777@gmail.com>
2019-04-28 12:19:42 +0300

    updated my repeat class and added a showDescription property

--- src/app/repeat.ts
+++ src/app/repeat.ts
@@ -1,3 +1,6 @@
 export class Repeat {
-  constructor(public id:number, public name:string,public description:string){}
+  public showDescription:boolean;
+  constructor(public id:number, public name:string,public description:string){
+    this.showDescription=false
+  }
 }
